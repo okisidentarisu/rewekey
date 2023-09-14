0f072b62c8a4cb20f1da878a34f58ff93c6bb05a
okisidentarisu <yuto_dama@yahoo.co.jp>
2023-09-14 11:53:35 +0000

    add: minimal api

---
+++ server/src/main.rs
@@ -0,0 +1,46 @@
+use agql::{
+    http::{playground_source, GraphQLPlaygroundConfig},
+    EmptySubscription, Schema,
+};
+use async_graphql as agql;
+use async_graphql_rocket::{GraphQLQuery, GraphQLRequest, GraphQLResponse};
+use model::{mutation::MutationRoot, query::QueryRoot, RewekeySchema};
+use rocket::{response::content, State};
+use sqlx::postgres::PgPoolOptions;
+
+mod db;
+mod model;
+
+#[rocket::get("/")]
+fn graphql_playground() -> content::RawHtml<String> {
+    content::RawHtml(playground_source(GraphQLPlaygroundConfig::new("/graphql")))
+}
+
+#[rocket::get("/graphql?<query..>")]
+async fn graphql_query(schema: &State<RewekeySchema>, query: GraphQLQuery) -> GraphQLResponse {
+    query.execute(schema).await
+}
+
+#[rocket::post("/graphql", data = "<request>", format = "application/json")]
+async fn graphql_request(
+    schema: &State<RewekeySchema>,
+    request: GraphQLRequest,
+) -> GraphQLResponse {
+    request.execute(schema).await
+}
+
+#[rocket::launch]
+async fn rocket() -> _ {
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect("postgres:///rewekey_dev")
+        .await
+        .unwrap();
+    let schema = Schema::build(QueryRoot, MutationRoot, EmptySubscription)
+        .data(pool)
+        .finish();
+    rocket::build().manage(schema).mount(
+        "/",
+        rocket::routes![graphql_query, graphql_request, graphql_playground],
+    )
+}
